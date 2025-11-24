33202caa41c7000f4eeae13b7c44960d6a21c374
himanshu9275 <hs6340872@gmail.com>
2025-11-24 12:37:44 +0530

    Create FactorsArray.java

---
+++ Arrays/FactorsArray.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class FactorsArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int maxFactor = 10;
+        int[] factors = new int[maxFactor];
+        int index = 0;
+
+        // Find factors
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                // Need more space? Then double the size
+                if (index == maxFactor) {
+                    maxFactor = maxFactor * 2;
+                    int[] temp = new int[maxFactor];
+                    for (int j = 0; j < index; j++) {
+                        temp[j] = factors[j];
+                    }
+                    factors = temp;
+                }
+                factors[index] = i;
+                index++;
+            }
+        }
+
+        // Display factors
+        System.out.print("Factors of " + number + " are: ");
+        for (int i = 0; i < index; i++) {
+            System.out.print(factors[i] + " ");
+        }
+        System.out.println();
+    }
+}
