4ca045e8d7679ab24d55e7d871faa8da9b6d2059
himanshu9275 <hs6340872@gmail.com>
2025-11-24 11:49:19 +0530

    Create FeetConversion.java

---
+++ Assignment02/FeetConversion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FeetConversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter distance in feet: ");
+        float feet = sc.nextFloat();
+
+        float yards = feet / 3;
+        float miles = yards / 1760;
+
+        System.out.println("The distance in yards is " + yards 
+                           + " while the distance in miles is " + miles);
+    }
+}
