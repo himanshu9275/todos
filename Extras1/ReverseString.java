4177db6ed98aa306116325779d4087990035fbf2
himanshu9275 <hs6340872@gmail.com>
2025-11-24 21:52:56 +0530

    Create ReverseString.java

---
+++ Extras1/ReverseString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed += s.charAt(i);
+        }
+
+        System.out.println("Reversed: " + reversed);
+    }
+}
