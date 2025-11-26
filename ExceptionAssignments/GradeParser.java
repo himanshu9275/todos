4762c41f927e0288dd163882c11053d81fd44487
himanshu9275 <hs6340872@gmail.com>
2025-11-26 21:21:32 +0530

    Create GradeParser.java

---
+++ ExceptionAssignments/GradeParser.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class GradeParser {
+    public static void main(String[] args) {
+        String[] grades = {"90", "A+", "85", "100", "B"};
+        for (String g : grades) {
+            try {
+                int val = Integer.parseInt(g);
+                System.out.println("Parsed grade: " + val);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid grade input: " + g);
+            }
+        }
+    }
+}
