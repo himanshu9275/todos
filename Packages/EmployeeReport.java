dd9cccc6a3fbaddcfb4404a4e00f518fcce52a04
himanshu9275 <hs6340872@gmail.com>
2025-11-26 22:44:08 +0530

    Create EmployeeReport.java

---
+++ Packages/EmployeeReport.java
@@ -0,0 +1,8 @@
+package com.company.analytics.hr;
+
+public class EmployeeReport {
+    public void printEmployeePerformance() {
+        System.out.println("Employee X: Excellent");
+        System.out.println("Employee Y: Good");
+    }
+}
