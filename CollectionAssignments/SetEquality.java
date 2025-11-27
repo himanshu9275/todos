1aa32ee2bcc333e12ef09e02bfc801e6215f0e79
himanshu9275 <hs6340872@gmail.com>
2025-11-27 11:51:25 +0530

    Create SetEquality.java

---
+++ CollectionAssignments/SetEquality.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class SetEquality {
+
+    public static void main(String[] args) {
+        Set<Integer> s1 = new HashSet<>(Arrays.asList(1,2,3));
+        Set<Integer> s2 = new HashSet<>(Arrays.asList(3,2,1));
+
+        System.out.println("Set1: " + s1);
+        System.out.println("Set2: " + s2);
+        System.out.println("Are equal? " + s1.equals(s2));
+    }
+}
