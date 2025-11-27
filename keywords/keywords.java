8f964b455009825856f0c6d4b083da94892e70e5
himanshu9275 <hs6340872@gmail.com>
2025-11-27 11:45:39 +0530

    Create keywords.java

---
+++ keywords/keywords.java
@@ -0,0 +1,27 @@
+ public String getName() {
+       return name;
+   }
+   public int getAge() {
+       return age;
+   }
+   public String getAilment() {
+       return ailment;
+   }
+   public String getPatientID() {
+       return patientID;
+   }
+  
+   public static void main(String[] args) {
+       // Set the hospital name (static variable)
+       Patient.setHospitalName("City Hospital");
+       // Create new patient instances
+       Patient patient1 = new Patient("Lathika", 30, "Flu", "P001");
+       Patient patient2 = new Patient("Lidiya", 45, "Fracture", "P002");
+       // Display total number of patients admitted
+       System.out.println("Total Patients Admitted: " + Patient.getTotalPatients());
+       // Display patient details
+       patient1.displayPatientDetails();
+       System.out.println();
+       patient2.displayPatientDetails();
+   }
+}
